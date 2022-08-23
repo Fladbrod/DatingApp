27d69c123e8980e5f749999f917d06eaf048d8ab
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2022-08-24 00:41:01 +0200

    End of section 7

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -19,13 +15,13 @@ namespace API.Controllers
 
         [Authorize]
         [HttpGet("auth")]
-        public ActionResult<string> GetSecret() 
+        public ActionResult<string> GetSecret()
         {
             return "secret text";
         }
 
         [HttpGet("not-found")]
-        public ActionResult<AppUser> GetNotFound() 
+        public ActionResult<AppUser> GetNotFound()
         {
             var thing = _context.Users.Find(-1);
 
@@ -35,7 +31,7 @@ namespace API.Controllers
         }
 
         [HttpGet("server-error")]
-        public ActionResult<string> GetServerError() 
+        public ActionResult<string> GetServerError()
         {
             var thing = _context.Users.Find(-1);
 
@@ -45,9 +41,9 @@ namespace API.Controllers
         }
 
         [HttpGet("bad-request")]
-        public ActionResult<string> GetBadRequest() 
+        public ActionResult<string> GetBadRequest()
         {
             return BadRequest("This was not a good request");
         }
     }
-}
+} 
---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        /* 
+        @params RequestDelegate What's coming up next in the middleware pipeline
+        @params ILogger displays the exception in the terminal window. We give it the type of ExceptionMiddleWare
+        @params IHostEnviroment what enviroment we're running in I.E production, development etc.
+        @params
+        */
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+            IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+
+        /*
+        Method that takes our response and formats it to Json.
+        */
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, "Internal Server Error");
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
