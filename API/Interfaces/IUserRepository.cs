eb21ac989596be198f1f87e39d51c26a5410ea1d
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2022-10-09 20:38:55 +0200

    Refactored to unit of work pattern + added modals

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -9,12 +9,12 @@ namespace API.Interfaces
         // Update isnt async since it only updates the tracking service in entity framework  to say that something has changed.
         // All the others are Task, so we need to await when using the methods.
         void Update(AppUser user);
-        Task<bool> SaveAllAsync();
         Task<IEnumerable<AppUser>> GetUsersAsync();
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByUsernameAsync(string username);
         Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
         Task<MemberDto> GetMemberAsync(string username);
+        Task<string> GetUserGender(string username);
 
     }
 }
